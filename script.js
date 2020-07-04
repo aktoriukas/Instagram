bb537d0c66e2eecb40503e7812d89fe0a75cb975
Gediminas Strumila <63856453+aktoriukas@users.noreply.github.com>
2020-07-04 13:41:17 +0100

    update on like button

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 // DISPLAY PICTURES
-
 {
     let html, newHtml;
 
     //html = '<div class="row"><div class="pic_%nr%"><div class="column"><img id=%nr% src="photo/%nr%.jpg"></div></div><div class="pic_%nr2%"><div class="column"><img id=%nr2% src="photo/%nr2%.jpg"></div></div><div class="pic_%nr3%"><div class="column"><img id=%nr3% src="photo/%nr3%.jpg"></div></div></div>'
-    html = '<div class="images"><div class="row"><div class="pic_%nr%"><div class="column"><img id="%nr%" src="photo/%nr%.jpg"><button id="button%nr%" class="like_button" type="button">pressMe</button></div></div><div class="pic_%nr2%"><div class="column"><img id="%nr2%" src="photo/%nr2%.jpg"><button id="button%nr2%" class="like_button" type="button">pressMe</button></div></div><div class="pic_%nr3%"><div class="column"><img id="%nr3%" src="photo/%nr3%.jpg"><button id="button%nr3%" class="like_button" type="button">pressMe</button></div></div></div>'
+    html = '<div class="images"><div class="row"><div class="pic_%nr%"><div class="column"><img id="%nr%" src="photo/%nr%.jpg"><button id="button%nr%" class="like_button" type="button"><img class="like_logo" src="images/like.png"></button></div></div><div class="pic_%nr2%"><div class="column"><img id="%nr2%" src="photo/%nr2%.jpg"><button id="button%nr2%" class="like_button" type="button"><img class="like_logo" src="images/like.png"></button></div></div><div class="pic_%nr3%"><div class="column"><img id="%nr3%" src="photo/%nr3%.jpg"><button id="button%nr3%" class="like_button" type="button"><img class="like_logo" src="images/like.png"></button></div></div></div>'
 
     for(let i = 1; i < 90; i+=3){
+
         newHtml = html.replace('%nr%', i);
         newHtml = newHtml.replace('%nr%', i);
         newHtml = newHtml.replace('%nr%', i);
         newHtml = newHtml.replace('%nr%', i);
-
 
         newHtml = newHtml.replace('%nr2%', i+1);
         newHtml = newHtml.replace('%nr2%', i+1);
@@ -40,16 +39,24 @@
 
    function selectImage(event) {
 
-    let picID, pictureID, buttonID;
+    let picID, pictureID, buttonID, buttonIDLike;
 
     picID = document.getElementById(event.target.id);
 
     // Check if pressed on picture
     if(event.target.id > 0 && event.target.id < 200){
 
-        buttonID = 'button' + event.target.id;
-        buttonID = document.getElementById(buttonID);
+        // Make like button visible
+
+        buttonIDLike = 'button' + event.target.id;
+        buttonID = document.getElementById(buttonIDLike);
         buttonID.style.visibility='visible';
+
+        // Add event listener to like button
+
+        buttonID.addEventListener('click' ,function(){
+            likeImage(buttonIDLike);
+        });
 
 
         //Check if it's been already pressed
@@ -77,19 +84,26 @@
    function expandImage(id){
        id.style.width='550px';
        id.style.border='6px solid rgb(125, 125, 125)';
-        // Make like button visible
-
 
    }
    function shrinkImage(id){
         id.style.width='300px';
         id.style.border='6px solid rgb(126, 116, 116)';
-
-}
+    }
    
 }
 
 // LIKE BUTTON
+{
+    function likeImage(buttonID){
+        console.log(`working id is ${buttonID}`);
+        addLike(buttonID);
+    }
+    function addLike(buttonID){
+        //TODO
+    }
+}
+
 // FLIP IMAGE TO SEE COMMENTS
 {
 
