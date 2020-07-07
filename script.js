454d3ea4da8c0f7df9595dc4a2c0f4ca12e1402c
Gediminas Strumila <63856453+aktoriukas@users.noreply.github.com>
2020-07-07 13:27:37 +0100

    like button obj

--- script.js
+++ script.js
@@ -4,7 +4,9 @@
 
     html = '<div class="images"><div class="row"><div class="pic_%nr%"><div class="column"><img id="%nr%" src="photo/%nr%.jpg"><button id="button%nr%" class="like_button" type="button"><img class="like_logo" src="images/like.png">&nbsp;<div></div></button></div></div><div class="pic_%nr2%"><div class="column"><img id="%nr2%" src="photo/%nr2%.jpg"><button id="button%nr2%" class="like_button" type="button"><img class="like_logo" src="images/like.png">&nbsp;<div></div></button></div></div><div class="pic_%nr3%"><div class="column"><img id="%nr3%" src="photo/%nr3%.jpg"><button id="button%nr3%" class="like_button" type="button"><img class="like_logo" src="images/like.png">&nbsp;<div></div></button></div></div></div>'
 
-    for(let i = 1; i < 90; i+=3){
+    // Hard coded number on images (to change)
+    let i;
+    for(i = 1; i < 90; i+=3){
 
         newHtml = html.replace('%nr%', i);
         newHtml = newHtml.replace('%nr%', i);
@@ -76,6 +78,7 @@
        let isAlreadyPressed;
        return isAlreadyPressed = ar.includes(id);
    }
+
    function expandImage(id){
     if (window.matchMedia('(min-width: 1000px)').matches) {
 
@@ -100,9 +103,17 @@
 
 // LIKE BUTTON
 {
+    class Instagram_image {
+        constructor(nr, likes) {
+            this.nr = nr,
+            this.likes = likes;
+        }
+    }
+    imageObj = makeImageObj(90);
+
     function likeImage(buttonID){
 
-        let logoID, disableButton
+        let logoID, disableButton, imageObj, imageObjNr;
 
         addLike(buttonID);
 
@@ -118,16 +129,38 @@
 
         disableButton = document.getElementById(buttonID);
         disableButton.disabled = true;
-        
+                
+        }
+
+    function makeImageObj(n) {
+        let instagramImage = {};
+
+        for (let i = 1; i <= n; i++) {
+            instagramImage[i] = new Instagram_image(i, 0)
+        }
+        return instagramImage;
     }
+
     function addLike(buttonID){
 
-        let likeCount, value;
+        let likeCount;
+
+
+        if(buttonID.length == 7){
+            imageObjNr = buttonID.slice(-1);
+        }else{
+            imageObjNr = buttonID.slice(-2);
+        }
+
+        imageObj[imageObjNr].likes++
 
         likeCount = document.getElementById(buttonID).children;
-    
-        likeCount[1].innerHTML = 1;
+        likeCount[1].innerHTML = imageObj[imageObjNr].likes;
+
+
     }
+
+
 }
 
 // FLIP IMAGE TO SEE COMMENTS
