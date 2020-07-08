5fbfc8b12b06a1cacb8fc328e0200acdd2ab12cf
Gediminas Strumila <63856453+aktoriukas@users.noreply.github.com>
2020-07-08 19:48:28 +0100

    every second image

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 // DISPLAY PICTURES
+
+class Instagram_image {
+    constructor(nr, likes, expand) {
+        this.nr = nr,
+        this.likes = likes;
+        this.expand = expand;
+    }
+}
+imageObj = makeImageObj(90);
+
+
 {
     let html, newHtml;
 
-    html = '<div class="images"><div class="row"><div class="pic_%nr%"><div class="column"><img id="%nr%" src="photo/%nr%.jpg"><button id="button%nr%" class="like_button" type="button"><img class="like_logo" src="images/like.png">&nbsp;<div></div></button></div></div><div class="pic_%nr2%"><div class="column"><img id="%nr2%" src="photo/%nr2%.jpg"><button id="button%nr2%" class="like_button" type="button"><img class="like_logo" src="images/like.png">&nbsp;<div></div></button></div></div><div class="pic_%nr3%"><div class="column"><img id="%nr3%" src="photo/%nr3%.jpg"><button id="button%nr3%" class="like_button" type="button"><img class="like_logo" src="images/like.png">&nbsp;<div></div></button></div></div></div>'
+    //html = '<div class="images"><div class="row"><div class="pic_%nr%"><div class="column"><img id="%nr%" src="photo/%nr%.jpg"><button id="button%nr%" class="like_button" type="button"><img class="like_logo" src="images/like.png">&nbsp;<div></div></button></div></div><div class="pic_%nr2%"><div class="column"><img id="%nr2%" src="photo/%nr2%.jpg"><button id="button%nr2%" class="like_button" type="button"><img class="like_logo" src="images/like.png">&nbsp;<div></div></button></div></div><div class="pic_%nr3%"><div class="column"><img id="%nr3%" src="photo/%nr3%.jpg"><button id="button%nr3%" class="like_button" type="button"><img class="like_logo" src="images/like.png">&nbsp;<div></div></button></div></div></div>'
+    html =  '<div class="pic_%nr%"><div class="column"><img class="img" id="%nr%" src="photo/%nr%.jpg"><button id="button%nr%" class="like_button" type="button"><img class="like_logo" src="images/like.png">&nbsp;<div></div></button></div></div>'
 
     // Hard coded number on images (to change)
-    let i;
-    for(i = 1; i < 90; i+=3){
+    for(i = 1; i <= 90; i++){
 
         newHtml = html.replace('%nr%', i);
         newHtml = newHtml.replace('%nr%', i);
         newHtml = newHtml.replace('%nr%', i);
         newHtml = newHtml.replace('%nr%', i);
-
-        newHtml = newHtml.replace('%nr2%', i+1);
-        newHtml = newHtml.replace('%nr2%', i+1);
-        newHtml = newHtml.replace('%nr2%', i+1);
-        newHtml = newHtml.replace('%nr2%', i+1);
-
-        newHtml = newHtml.replace('%nr3%', i+2);
-        newHtml = newHtml.replace('%nr3%', i+2);
-        newHtml = newHtml.replace('%nr3%', i+2);
-        newHtml = newHtml.replace('%nr3%', i+2);
                 
         document.querySelector('.images').insertAdjacentHTML('beforeend', newHtml);
     }
@@ -31,49 +32,50 @@
 {
     var pressed = [];
 
+    expandEverySecondImage();
    document.querySelector('.images').addEventListener('click', selectImage);
 
 
-   function selectImage(event) {
+    function selectImage(event) {
 
-    let picID, pictureID, buttonID, buttonIDLike;
+        let picID, pictureID, buttonID, buttonIDLike;
 
-    picID = document.getElementById(event.target.id);
+        picID = document.getElementById(event.target.id);
 
-    // Check if pressed on picture
-    if(event.target.id > 0 && event.target.id < 200){
+        // Check if pressed on picture
+        if(event.target.id > 0 && event.target.id < 200){
 
-        // Make like button visible
+            // Make like button visible
 
-        buttonIDLike = 'button' + event.target.id;
-        buttonID = document.getElementById(buttonIDLike);
-        buttonID.style.visibility='visible';
+            buttonIDLike = 'button' + event.target.id;
+            buttonID = document.getElementById(buttonIDLike);
+            buttonID.style.visibility='visible';
 
-        // Add event listener to like button
+            // Add event listener to like button
 
-        buttonID.addEventListener('click' ,function(){
-            likeImage(buttonIDLike);
-        });
-
-
-        //Check if it's been already pressed
-
-        if (itItAlreadyPressed(pressed, event.target.id)){
-
-            pictureID = pressed.findIndex(cur => cur == event.target.id);
-            delete pressed[pictureID];
-            shrinkImage(picID);
-            buttonID.style.visibility='hidden';
+            buttonID.addEventListener('click' ,function(){
+                likeImage(buttonIDLike);
+            });
 
 
-        }else{
-            pressed.push(event.target.id);
-            expandImage(picID);
+            //Check if it's been already pressed
 
-        }        
+            if (itItAlreadyPressed(pressed, event.target.id)){
+
+                pictureID = pressed.findIndex(cur => cur == event.target.id);
+                delete pressed[pictureID];
+                shrinkImage(picID);
+                buttonID.style.visibility='hidden';
+
+
+            }else{
+                pressed.push(event.target.id);
+                expandImage(picID);
+
+            }        
+        };
     };
-   };
-   
+    
    function itItAlreadyPressed(ar, id){
        let isAlreadyPressed;
        return isAlreadyPressed = ar.includes(id);
@@ -97,19 +99,28 @@
    function shrinkImage(id){
         id.style.width='300px';
         id.style.border='6px solid rgb(126, 116, 116)';
+
+    }
+    function expandEverySecondImage(){
+        let i, imageElement;
+        for(i = 2; i < 90; i = i+=3){
+            imageElement = document.getElementById(i);
+            imageElement.style.width='600px';
+            imageObj[i].expand=true;
+            i++;
+            imageElement = document.getElementById(i);
+            imageElement.style.width='600px';
+            imageObj[i].expand=true;
+        }
     }
    
 }
 
 // LIKE BUTTON
 {
-    class Instagram_image {
-        constructor(nr, likes) {
-            this.nr = nr,
-            this.likes = likes;
-        }
+    function setExpand(){
+        console.log('working');
     }
-    imageObj = makeImageObj(90);
 
     function likeImage(buttonID){
 
@@ -130,16 +141,8 @@
         disableButton = document.getElementById(buttonID);
         disableButton.disabled = true;
                 
-        }
-
-    function makeImageObj(n) {
-        let instagramImage = {};
-
-        for (let i = 1; i <= n; i++) {
-            instagramImage[i] = new Instagram_image(i, 0)
-        }
-        return instagramImage;
     }
+
 
     function addLike(buttonID){
 
@@ -166,4 +169,12 @@
 // FLIP IMAGE TO SEE COMMENTS
 {
 
+}
+function makeImageObj(n) {
+    let instagramImage = {};
+
+    for (let i = 1; i <= n; i++) {
+        instagramImage[i] = new Instagram_image(i, 0, false)
+    }
+    return instagramImage;
 }
