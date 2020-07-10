8c8d2e8b61bf4ce9051c3f5f5d1423b30ce71fe7
Gediminas Strumila <63856453+aktoriukas@users.noreply.github.com>
2020-07-10 17:33:07 +0100

    expand image/display button

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-// DISPLAY PICTURES
 
 class Instagram_image {
-    constructor(nr, likes, expand) {
+    constructor(nr, likes, expand, visibleLike) {
         this.nr = nr,
         this.likes = likes;
-        this.expand = expand;
+        this.expand = expand,
+        this.visibleLike = false
     }
 }
-imageObj = makeImageObj(90);
+const nrOFimages = 90;
+imageObj = makeImageObj(nrOFimages);
 
-
+// DISPLAY PICTURES
 {
     let html, newHtml;
 
     html =  '<div class="pic_%nr%"><div class="column"><img class="img" id="%nr%" src="photo/%nr%.jpg"><button id="button%nr%" class="like_button" type="button"><img class="like_logo" src="images/like.png">&nbsp;<div></div></button></div></div>'
 
     // Hard coded number on images (to change)
-    for(i = 1; i <= 90; i++){
+    for(i = 1; i <= nrOFimages; i++){
 
         newHtml = html.replace('%nr%', i);
         newHtml = newHtml.replace('%nr%', i);
@@ -32,53 +33,127 @@ imageObj = makeImageObj(90);
     var pressed = [];
 
     expandEverySecondImage();
+    checkIfDisplayLikeButton();
    document.querySelector('.images').addEventListener('click', selectImage);
 
 
     function selectImage(event) {
 
-        let picID, buttonID, buttonIDLike, eventID;
+        let picElement, picElementChange, likeButtonElement, likeButtonID, eventID;
 
-        picID = document.getElementById(event.target.id);
 
         // Check if pressed on picture
-        if(event.target.id > 0 && event.target.id < 200){
+        if(event.target.id > 0 && event.target.id <= nrOFimages){
+            picElement = document.getElementById(event.target.id);
+
 
             // Make like button visible
 
-            buttonIDLike = 'button' + event.target.id;
-            buttonID = document.getElementById(buttonIDLike);
-            buttonID.style.visibility='visible';
+            // likeButtonID = 'button' + event.target.id;
+            // likeButtonElement = document.getElementById(likeButtonID);
+            // likeButtonElement.style.visibility='visible';
 
-            // Add event listener to like button
+            // // Add event listener to like button
 
-            buttonID.addEventListener('click' ,function(){
-                likeImage(buttonIDLike);
-            });
+            // likeButtonElement.addEventListener('click' ,function(){
+            //     likeImage(likeButtonID);
+            // });
 
 
             eventID = event.target.id;
+            eventID = parseInt(eventID);
 
             //Check if expand == true
-            if(isEven(eventID)){
-                if (imageObj[eventID].expand == false){
-                    expandImage(picID);
-                    imageObj[eventID].expand = true;
-                }else{
-                    shrinkImage(picID);
-                    buttonID.style.visibility='hidden';
-                    imageObj[eventID].expand = false;
-                    }
-    
-                } 
-            }else{
+            if(isEven(eventID) && imageObj[eventID].expand == true){
 
-            }      
-        };
+                shrinkImage(picElement);
+                imageObj[eventID].expand=false;
+                imageObj[eventID].visibleLike=false;
+                
+                picElementChange = document.getElementById(eventID -1);
+
+                expandImage(picElementChange);
+                imageObj[eventID-1].expand=true;
+                imageObj[eventID-1].visibleLike=true;
+
+            }
+            else if (isEven(eventID) && imageObj[eventID].expand == false){
+
+                expandImage(picElement);
+                imageObj[eventID].expand=true;
+                imageObj[eventID].visibleLike=true;
+
+                picElementChange = document.getElementById(eventID -1);
+
+                shrinkImage(picElementChange);
+                imageObj[eventID-1].expand=false;
+                imageObj[eventID-1].visibleLike=false;
+
+            }
+            else if (!isEven(eventID) && imageObj[eventID].expand == true){
+
+                shrinkImage(picElement);
+                imageObj[eventID].expand=false;
+                imageObj[eventID].visibleLike=false;
+
+                picElementChange = document.getElementById(eventID +1);
+
+                expandImage(picElementChange);
+                imageObj[eventID+1].expand=true;
+                imageObj[eventID+1].visibleLike=true;
+
+            }
+            else if (!isEven(eventID) && imageObj[eventID].expand == false){
+
+                expandImage(picElement);
+                imageObj[eventID].expand=true;
+                imageObj[eventID].visibleLike=true;
+
+                picElementChange = document.getElementById(eventID +1);
+
+                shrinkImage(picElementChange);
+                imageObj[eventID+1].expand=false;
+                imageObj[eventID+1].visibleLike=false;
+
+            }
+            checkIfDisplayLikeButton();
+
+        }
+        else {
+            console.log(event.target);
+        }
     };
+    function checkIfDisplayLikeButton(){
+        let i;
+
+        for(i=1; i < nrOFimages; i++){
+            if(imageObj[i].visibleLike == true){
+                displayLikeButton(i);
+
+            // likeButtonElement.addEventListener('click' ,function(){
+            //     likeImage(likeButtonID);
+            // });
+
+            }
+            else if(imageObj[i].visibleLike == false){
+                hideLikeButton(i);
+            }
+        }
+    }
 
     function isEven(n) {
         return n % 2 == 0;
+    }
+    function hideLikeButton(i){
+        likeButtonID = 'button' + i;
+        likeButtonElement = document.getElementById(likeButtonID);
+        likeButtonElement.style.visibility='hidden';
+    }
+    function displayLikeButton(i){
+        likeButtonID = 'button' + i;
+        likeButtonElement = document.getElementById(likeButtonID);
+        likeButtonElement.style.visibility='visible';
+        }
     }
      
     
@@ -104,19 +179,21 @@ imageObj = makeImageObj(90);
     }
     function expandEverySecondImage(){
         let i, imageElement;
-        for(i = 2; i < 90; i = i+=3){
+        for(i = 2; i < nrOFimages; i = i+=3){
             imageElement = document.getElementById(i);
             imageElement.style.width='600px';
             imageObj[i].expand=true;
+            imageObj[i].visibleLike=true;
+
             i++;
             imageElement = document.getElementById(i);
             imageElement.style.width='600px';
             imageObj[i].expand=true;
+            imageObj[i].visibleLike=true;
+
         }
     }
    
-
-
 // LIKE BUTTON
 {
 
@@ -125,6 +202,7 @@ imageObj = makeImageObj(90);
         let logoID, disableButton, imageObj, imageObjNr;
 
         addLike(buttonID);
+
 
         // 1. Get child element of button (Like_logo) 
 
